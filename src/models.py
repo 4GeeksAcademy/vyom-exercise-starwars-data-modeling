07c248ed8b5ff27dfcc3ef4890342e094fcb46cd
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-08-27 20:08:21 +0000

    practicando python con flaskSQL para crear base de datos

--- src/models.py
+++ src/models.py
@@ -7,26 +7,58 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(100), nullable=False, unique=True)
+    email = Column(String(100), nullable=False, unique=True)
+    registration_date = Column(String(50), nullable=True)
+    password = Column(String(100), nullable=False)
+    favorites = relationship("Favorites", backref="user", lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(100), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=False)
+    planet = relationship('Planets', backref='characters', lazy=True)
+    starships = relationship('Starships', backref='character', lazy=True)
 
-    def to_dict(self):
-        return {}
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    characters = relationship('Characters', backref='planet', lazy=True)
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=False)
+    character = relationship('Characters', backref='starships', lazy=True)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=False)
+    starship_id = Column(Integer, ForeignKey('starships.id'), nullable=False)
+
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+#     def to_dict(self):
+#         return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
